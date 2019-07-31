e8bb61faf2ad549ec58682adeafdd6e4f0df9249
Trofin Adrian <adrian.trofin@softvision.ro>
2019-07-31 17:56:55 +0300

    some progress on react store

--- ReactStore/amazingstore/src/Components/MainContent/MainContent.jsx
+++ ReactStore/amazingstore/src/Components/MainContent/MainContent.jsx
@@ -6,15 +6,16 @@ class MainContent extends React.Component {
        super(props);
        this.state = {
            categories: {
-               face: ['CREAMS & GELS', 'Patches', 'Emulsions'],
-               body: ['aaa','bbb'],
-               foot: [],
-               hands: [],
-               hair: [],
-               makeup: []
+               face: ['CREAMS & GELS', 'Patches', 'Emulsions','Toners','Face Cleaning','Make Up Removal','Scrubs','Peeling','Masks'],
+               body: ['test-body'],
+               foot: ['test-foot'],
+               hands: ['test-hands'],
+               hair: ['test-hair'],
+               makeup: ['test-makeup']
            }
        }
    }
+
 
    render() {
        return (
--- ReactStore/amazingstore/src/Components/Searchbar/Searchbar.jsx
+++ ReactStore/amazingstore/src/Components/Searchbar/Searchbar.jsx
@@ -4,14 +4,12 @@ import './Searchbar.css';
 
 const Searchbar = () => {
     return (
-<div className="header-bottom">
             <div className="searchBar">
                 <i className="fas fa-search"></i>
                 
                 <input type="text" placeholder="Search products by SKU or name"></input>
                 </div>
             
- </div>
         )
 }
 
